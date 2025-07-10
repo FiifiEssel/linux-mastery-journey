097d9197f50b209af407703acc2479c8efa31b50
FiifiEssel <ekessel804@gmail.com>
2025-07-10 17:12:10 +0000

    Updated the bash script to inlude date and logged in users

--- init_report.sh
+++ init_report.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 echo "Red Team Lab Bootstrapped"
-echo %(who) >lab_status.txt
+echo "Date: $(date)"
+echo "Logged in Users: $(who)"
+
+echo "................................."
+{
+	echo "................................"
+	echo "Red Team Lab Bootstrapped"
+	echo "Date: $(date)"
+	echo "Logged In User: $(who)"
+
+} >> lab_status.txt
 
